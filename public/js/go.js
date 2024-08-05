dc84036f82823caa645ff8bcd621b14c48fc7f59
�東�京� <131155313+konomi-tokyo@users.noreply.github.com>
2024-08-05 17:12:45 +0900

    Update go.js

--- public/js/go.js
+++ public/js/go.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", function() {
+ window.addEventListener("load", function() {
 var search = document.getElementById("search")
 search.addEventListener("keyup", function(event) {
     event.preventDefault()
